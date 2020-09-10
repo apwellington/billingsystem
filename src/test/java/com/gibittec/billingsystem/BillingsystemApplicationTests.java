3d2cff6cff081a3f296f48dcf5b0b827f9b597c6
Wellington <wellington.adames.p@gmail.com>
2020-09-09 23:40:22 -0400

    Initial commit

---
+++ src/test/java/com/gibittec/billingsystem/BillingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibittec.billingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
